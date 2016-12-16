543f590ebe553c4c1abf264955ff10a8ffb33beb
Jasminesaur <jnjasmine@yahoo.com>
2016-12-16 12:31:18 -0800

    Fixed Responsiveness, Part 2

--- js/highlight.js
+++ js/highlight.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
+	var myElem = document.getElementById('tabs');
+	if(myElem != null){
 	$( "#tabs" ).tabs();
+	} else{
 	$( "#tabs-3" ).tabs();
 	$( "#tabs-2" ).tabs();
+	}
 });
